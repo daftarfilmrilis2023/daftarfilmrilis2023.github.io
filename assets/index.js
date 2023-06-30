82712abe3a6740048cccacc839c8162cdea37c3d
daftarfilmrilis2023 <138224140+daftarfilmrilis2023@users.noreply.github.com>
2023-06-30 20:15:33 +0700

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+// const audio = new Audio("../assets/One Direction - What Makes You Beautiful.mp3");
+// console.log("OIASJDGFIOSJDGF")
+
+// const buttons = document.querySelectorAll(".playButton");
+
+
+// buttons.forEach(button => {
+//     button.addEventListener("click", () => {
+//         console.log("CLICKED");
+//       audio.play();
+//     });
+//   });
